48ba4c18b307c156b56a560d9c02c8a41feaae3f
laura <rosechebet1997@gmail.com>
2022-08-08 00:09:52 -0700

    base test

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
-"""Testing the BaseModel"""
+"""test BaseModel"""
+
 import unittest
 import os
 from models.base_model import BaseModel
 import pep8
+
 
 class TestBaseModel(unittest.TestCase):
     """Test BaseModel"""
@@ -13,24 +15,27 @@ class TestBaseModel(unittest.TestCase):
 
     def test_pep8_BaseModel(self):
         """Testing for pep8"""
+
         style = pep8.StyleGuide(quiet=True)
         p = style.check_files(['models/base_model.py'])
         self.assertEqual(p.total_errors, 0, "Check pep8")
 
-
     def test_save_BaesModel(self):
         """Test save_Basemodel"""
+
         self.base.save()
         self.assertNotEqual(self.base.created_at, self.base.updated_at)
 
     def test_doc(self):
         """ Tests doc """
+
         self.assertisNotNone(BaseModel.__doc__)
 
     def test_to_json(self):
-        '''Test to jason'''
+        '''Test to json'''
 
     def test_kwarg(self):
+        """Test to kwarg"""
         basemodel = BaseModel(name="base")
         self.assertEqual(type(basemodel).__name__, "BaseModel")
         self.assertFalse(hasattr(basemodel, "id"))
@@ -38,6 +43,7 @@ class TestBaseModel(unittest.TestCase):
         self.assertTrue(hasattr(basemodel, "name"))
         self.assertFalse(hasattr(basemodel, "updated_at"))
         self.assertTrue(hasattr(basemodel, "__class__"))
+
 
 if __name__ == "__main__":
     unittest.main()
